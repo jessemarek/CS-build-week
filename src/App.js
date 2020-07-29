b557253dbfacea278649f3f758cd9942e39b5a56
unknown <dethgrenade@gmail.com>
2020-07-29 08:45:39 -0700

    updated comments in App.js. Added range slider input to control simulation speed

--- src/App.js
+++ src/App.js
@@ -4,47 +4,66 @@ import { makeGrid, fillGrid, randInitCells, lifecycle } from './game-logic/funct
 import CellBtn from './components/CellBtn'
 
 function App() {
+  // init variables
   let numCols = 25
   let numRows = 25
 
   const initGrid = makeGrid(numCols, numRows)
   fillGrid(initGrid)
 
+  // STATE **********************************************************************************************************************
   const [grid, setGrid] = useState(initGrid)
   const [gameIsRunning, setGameIsRunning] = useState(false)
   const [genNum, setGenNum] = useState(0)
+  const [gameSpeed, setGameSpeed] = useState(60)
 
+  // REFS ***********************************************************************************************************************
   const runningRef = useRef(gameIsRunning)
   runningRef.current = gameIsRunning
 
+  const speedRef = useRef(gameSpeed)
+  speedRef.current = gameSpeed
+
+  // CALLBACKS ******************************************************************************************************************
   const runSimulation = useCallback(() => {
+    // if the stop button is clicked break out of the loop
     if (!runningRef.current) {
       return
     }
-
+    // update the generation number
     setGenNum(g => g + 1)
+    // advance the lifecycle
     setGrid(g => lifecycle(g))
 
-    setTimeout(runSimulation, 16.67)
+    // run the simulation at the speed specififed
+    setTimeout(runSimulation, (1000 / speedRef.current))
 
   }, [])
 
+  // starts the simulation until the stop button is clicked
   const clickPlay = e => {
     e.preventDefault()
 
+    // toggle to state to the opposite when start button is clicked
     setGameIsRunning(!gameIsRunning)
+
+    // set the ref to true to prevent race case of new render beating the update of the state
     if (!gameIsRunning) {
       runningRef.current = true
+
+      // start the simulation loop
       runSimulation()
     }
   }
 
+  // Fills the grid with a random selection of alive cells
   const clickRandom = e => {
     e.preventDefault()
 
     setGrid(randInitCells(grid))
   }
 
+  // Advances the game window to the next generation
   const clickNextGen = e => {
     e.preventDefault()
 
@@ -52,12 +71,22 @@ function App() {
     setGenNum(genNum + 1)
   }
 
+  // Clears the game window and resets the generation number
   const resetGame = e => {
     e.preventDefault()
 
     setGenNum(0)
     setGrid(initGrid)
   }
+
+  // Adjusts simulation render speed (frame rate)
+  const sliderChange = e => {
+    e.preventDefault()
+
+    setGameSpeed(e.target.value)
+  }
+
+  // ****************************************************************************************************************************
 
   return (
     <div className="App">
@@ -80,10 +109,16 @@ function App() {
           }
         </div>
         <div className="game-btns">
-          <button onClick={clickPlay}>{gameIsRunning ? 'Pause' : 'Play'}</button>
+          <button onClick={clickPlay}>{gameIsRunning ? 'Stop' : 'Start'}</button>
           <button disabled={gameIsRunning} onClick={clickNextGen}>Next Gen</button>
           <button disabled={gameIsRunning} onClick={clickRandom}>Randomize</button>
           <button disabled={gameIsRunning} onClick={resetGame}>Reset</button>
+        </div>
+        <div className="speed-ctrl">
+          <label>Simulation Speed
+            <input type="range" min="1" max="120" value={gameSpeed} onChange={sliderChange} />
+            <p>{`${gameSpeed} Frames per second`}</p>
+          </label>
         </div>
       </div>
     </div>
